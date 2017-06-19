c65d6e8af714b0851fbd30250410a5107eb8663d
Fauzan Ardhana <draxknight@gmail.com>
2017-06-19 22:37:06 +0700

    Create echo.php

---
+++ echo.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+require_once('class.php');
+$channelAccessToken = 'i2g1ajRXqBIieiy1V0dx2xltyT73roFSiDKoHM4BeUKgd+a4Ik/P+Vp6iON18XQPeWJVRexzGBa6+zijuhSJHZni3cEzafMlihthHrnrpMe5UrNOPAFs7Nsw9mQ/DVtfpns3zVmMc14l8T10LWJn4wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'c5e8a89066b2822d0815d020ab58e6f5';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => 'Hello World!'
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
